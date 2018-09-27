eb66d31e294b9b4e39aa0bd9a75e076a4a36803d
wuyunfang <wuyunfang@minzhetech.com>
2018-09-27 17:31:11 +0800

    update toc

--- source/js/fox.js
+++ source/js/fox.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function($){
 
     "use strict";
 
+    var initTop = 0,staticHeight = 65;
+
     var loader = function() {
         $(window).on("load", function() {
             $(".status").fadeOut();
@@ -11,15 +13,68 @@ jQuery(document).ready(function($){
 
     /* 滚动条 */
     var scroll = function () {
-        $(window).on('scroll',function(){
-            /* 顶部菜单样式切换 */
-            var height = document.documentElement.scrollTop || document.body.scrollTop;
-            var opcaity=(height/300>1)?1:height/300;
+        $(window).scroll(function (){
+            var currentTop = $(this).scrollTop();
+            var opcaity = (currentTop/250>1) ? 1:currentTop/250 <=0 ? .2 : currentTop/250;
             $('.header').css('background','rgba(255,255,255,'+opcaity+')');
-            /* 文章阅读进度 */
-            progress();
+
+            /* 滚动条高度判断 是否显示文章目录和go-up */
+            if (currentTop > staticHeight) {
+                $('#go-up').fadeIn();
+                $('.post-page-title').show();
+            }else{
+                $('#go-up').fadeOut();
+                $('.post-page-title').hide();
+            }
+
         });
-    };
+    }
+
+    $('#go-up').on('click', function () {
+        console.log('go');
+        $('html,body').animate({scrollTop:0}, 500);
+    });
+
+    /* hash change */
+    $(window).on('hashchange',function(){
+        var target = $(decodeURI(location.hash)),top = target.offset().top-65;
+        $('html,body').animate({scrollTop:top}, 500);
+        $('.toc-link[href="'+decodeURI(location.hash)+'"]').addClass('active')
+        $('.toc-link[href!="'+decodeURI(location.hash)+'"]').removeClass('active');
+    });
+
+    /* 判断滚动条下拉还是上拉 */
+    var scrollDirection = function (currentTop) {
+        var result = currentTop > initTop // true is down & false is up
+        initTop = currentTop
+        return result
+    }
+
+    // var scroll = function () {
+    //     $(window).on('scroll',function(){
+    //         /* 顶部菜单样式切换 */
+    //         var height = document.documentElement.scrollTop || document.body.scrollTop;
+    //         var opcaity=(height/300>1)?1:height/300;
+    //         $('.header').css('background','rgba(255,255,255,'+opcaity+')');
+    //
+    //         /* 跟随滚动条切换选中目录 */
+    //         var list = $('.article-content').find('h1,h2,h3,h4,h5,h6'),activeId = '',top = $(this).scrollTop()
+    //         list.each(function () {
+    //             var head = $(this);
+    //             if (top > head.offset().top - 65) {
+    //                 activeId = '#' + $(this).attr('id')
+    //             }
+    //         });
+    //         var currentActive = $('.toc-link.active');
+    //         if (activeId && currentActive.attr('href') !== activeId) {
+    //             location.hash = decodeURI(activeId);
+    //             $('.toc-link[href="'+decodeURI(location.hash)+'"]').addClass('active')
+    //             $('.toc-link[href!="'+decodeURI(location.hash)+'"]').removeClass('active');
+    //         }
+    //         /* 文章阅读进度 */
+    //         progress();
+    //     });
+    // };
 
     /* 文章toc 点击错位解决 */
     var hashchange = function (){
@@ -109,8 +164,8 @@ jQuery(document).ready(function($){
     (function init() {
         loader();
         scroll();
-        hashchange();
-        progress();
+        // hashchange();
+        // progress();
         // Annie_Nav();
         // Annie_ToTop();
         // Annie_Toc();
